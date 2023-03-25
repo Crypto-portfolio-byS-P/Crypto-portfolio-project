c2ae932573a07cff53410e4f32672a97597e3a40
Paulius Mockevicius <pmockevicius@gmail.com>
2023-03-25 19:01:26 +0100

    add form to each coin

--- routes/crypto.routes.js
+++ routes/crypto.routes.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Coin = require('../models/Coin.model');
+const mongoose = require("mongoose");
 
 router.get("/charts",  (req, res) => {
     // res.render("views/index");
-    res.send("hello")
+    res.render("charts")
   });
 
-  router.get("/news", (req, res) => {
-    res.render("views/news");
+  router.get("/portfolio", (req, res) => {
+    res.render("portfolio/portfolio");
   });
+
+
+// Add to portfolio: Display form
+
+router.post('/portfolio', (req, res, next) => {
+
+  console.log(req.body)
+  const coin = {
+   name: req.body.coinName,
+   owned: req.body.amount,
+   coinId: req.body.coinId
+  }
+
+  Coin.create(coin)
+  .then(coinInfoFromDb =>{
+    console.log(coinInfoFromDb)
+    res.send("Coin Created")
+  })
+})
 
 module.exports = router;

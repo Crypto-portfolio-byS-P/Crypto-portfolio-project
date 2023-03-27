cbd4a73d17d050fd6087974242ecdb5ce1e80f3b
Sophie White <sophie.white@mordrid.net>
2023-03-27 10:23:32 +0200

    Add update and delete

--- models/Coin.model.js
+++ models/Coin.model.js
@@ -1,28 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-const coinSchema = new Schema(
-  {
-    name: {
-      type: String,
-    },
-    owned: {
-      type: Number,
-    },
-    coinId: {
-      type: String,
-    },
-    addedBy: {
-      type: String,
-    },
-    purchasedAt: {
-      type: Number,
-    },
-    image: {
-      type: String,
-    }
-    
-  }
-);
+const coinSchema = new Schema({
+  name: {
+    type: String,
+  },
+  owned: {
+    type: Number,
+  },
+  coinId: {
+    type: String,
+  },
+  addedBy: {
+    type: String,
+  },
+  purchasedAt: {
+    type: Number,
+  },
+  image: {
+    type: String,
+  },
+});
 
 const Coin = model("Coin", coinSchema);
 
--- routes/crypto.routes.js
+++ routes/crypto.routes.js
@@ -1,55 +1,74 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Coin = require('../models/Coin.model');
+const Coin = require("../models/Coin.model");
 const mongoose = require("mongoose");
+const isLoggedIn = require("../middleware/isLoggedIn");
 
 // let currentUserEmail
 
-router.get("/charts",  (req, res) => {
-    // res.render("views/index");
-    res.render("charts")
-  });
-
-  // router.get("/portfolio", (req, res) => {
-  //   res.render("portfolio/portfolio");
-  // });
-
+router.get("/charts", (req, res) => {
+  // res.render("views/index");
+  res.render("charts");
+});
 
 // Add to portfolio: Display form
 
-router.post('/portfolio/add', (req, res, next) => {
-
-  // console.log(req.session.currentUser)
-
-  
+router.post("/portfolio/add", (req, res, next) => {
   const coin = {
-   name: req.body.coinName,
-   owned: req.body.amount,
-   coinId: req.body.coinId,
-   purchasedAt: req.body.purchasedAt,
-   addedBy: req.session.currentUser.email,
-   image: req.body.image
-  }
+    name: req.body.coinName,
+    owned: req.body.amount,
+    coinId: req.body.coinId,
+    purchasedAt: req.body.purchasedAt,
+    addedBy: req.session.currentUser.email,
+    image: req.body.image,
+  };
 
-  Coin.create(coin)
-  .then(coinInfoFromDb =>{
+  Coin.create(coin).then((coinInfoFromDb) => {
     // console.log(coinInfoFromDb)
-    res.redirect("/crypto/portfolio")
-    res.render("portfolio/portfolio", {coinInfoFromDb})
-    
-  })
-})
+    res.redirect("/crypto/portfolio");
+    res.render("portfolio/portfolio", { coinInfoFromDb });
+  });
+});
 
+router.get(`/portfolio`, (req, res, next) => {
+  console.log(req.session);
 
-router.get('/portfolio', (req, res, next) => {
+  Coin.find({ addedBy: req.session.currentUser.email }).then(
+    (coinsInfoFromDb) => {
+      res.render(`portfolio/portfolio`, { coin: coinsInfoFromDb });
+    }
+  );
+});
 
- 
-console.log(req.session
-  )
+//GET Edit coin
+router.get(`/:coinId/edit`, (req, res, next) => {
+  const { coinId } = req.params;
 
-  Coin.find({addedBy: req.session.currentUser.email})
-  .then(coinsInfoFromDb =>{
-    res.render("portfolio/portfolio", {coin: coinsInfoFromDb})
-  })
-})
+  Coin.findById(coinId)
+    .then((coinToEdit) => {
+      console.log(coinToEdit);
+      res.render("/portfolio/coin-update", {coinToEdit });
+    })
+    .catch((error) => next(error));
+});
+
+//POST Edit coin
+router.post(`/:coinId/edit`, (req, res, next) => {
+  const { coinId } = req.params;
+  const { owned, purchasedAt } = req.body;
+
+  Coin.findByIdAndUpdate(coinId, { owned, purchasedAt }, { new: true })
+    .then(() => res.redirect(`/portfolio`))
+    .catch((error) => next(error));
+});
+
+//POST Delete coin
+router.post(`/:coinId/delete`, (req, res, next) => {
+  const { coinId } = req.params;
+
+  Coin.findByIdAndDelete(coinId)
+    .then(() => res.redirect("/portfolio"))
+    .catch((error) => next(error));
+});
+
 module.exports = router;

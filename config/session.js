e6b6e10027ccb046cad9caf2129c6bec2282a478
Paulius Mockevicius <pmockevicius@gmail.com>
2023-03-28 16:53:09 +0200

    Store sessions to the correct place in DB

--- config/session.js
+++ config/session.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
                 maxAge: 1000 * 60 * 60 * 24,
             },
             // store: MongoStore.create({
-            //     mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1/',
+            //     mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1/crypto-link',
             // }),
         }),
     );
